81fe6044f1e6dcaa678102b82fda53b12725478a
Lloyd <lloydjvrensburg@gmail.com>
2020-11-21 13:35:16 +0200

    Wildfire Tracker Final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Map from "./components/Map";
+import Loader from "./components/Loader.js";
+import Header from "./components/Header";
 
 function App() {
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      // Set loading to true before calling API
+      setLoading(true);
+
+      // Call API and save in res
+      const res = await fetch(
+        "https://eonet.sci.gsfc.nasa.gov/api/v2.1/events"
+      );
+
+      // res.json to destructer events field out of JSON object
+      const { events } = await res.json();
+
+      setEventData(events);
+      setLoading(false);
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      {!loading ? <Map eventData={eventData} /> : <Loader />}
     </div>
   );
 }
